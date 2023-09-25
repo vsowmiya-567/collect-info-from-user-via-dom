4b79594874093306db65117e139820c6ec433935
vsowmiya-567 <139351223+vsowmiya-567@users.noreply.github.com>
2023-09-25 22:52:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function submit(){
+    let a= document.getElementById("fstname").value;
+    let b= document.getElementById("mdlname").value;
+    let c= document.getElementById("lstname").value;
+    let d= document.getElementById("email").value;
+    let e= document.getElementById("pincode").value;
+
+
+
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    console.log(d)
+    console.log(e)
+
+}
